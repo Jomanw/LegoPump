2e15caea108414c577f15cb8e729e71fe524a143
Jordan Wick <wickj@mit.edu>
2018-05-08 16:59:49 -0400

    Added server and webpage code from previous project.
    Will modify to fit the current application.

---
+++ app.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var ws281x = require('rpi-ws281x-native');
+var bodyParser = require('body-parser');
+var cp = require('child_process');
+var light_process = cp.fork('child.js');
+var sleep = require('sleep');
+var fs = require('fs');
+
+var NUM_LEDS = parseInt(process.argv[2], 10) || 100,
+	 pixelData = new Uint32Array(NUM_LEDS);
+ws281x.init(NUM_LEDS);
+
+var lightsMoving = false;
+
+/*
+redData = new Uint32Array(NUM_LEDS);
+for(var i=0;i<NUM_LEDS;i++) {
+	redData[i] = 0xff0000;
+}
+blueData = new Uint32Array(NUM_LEDS);
+for(var i=0;i<NUM_LEDS;i++) {
+	blueData[i] = 0x00ff00;
+}
+greenData = new Uint32Array(NUM_LEDS);
+for(var i=0;i<NUM_LEDS;i++) {
+	greenData[i] = 0x0000ff;
+}
+*/
+
+// Current Idea: Use Child Processes.
+
+//Function for extracting HSL values from RGB
+/*
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+*/
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+var mode = "random";  // Mode currently in
+var primary_color = 0x00ff00;
+var secondary_color = 0xff00ff;
+
+app.get('/', function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	console.log('check');
+	console.log(req.method);
+	fs.readFile('./form.html', function(err,data) {
+		res.write(data);
+		res.end();
+	});
+});
+
+app.post('/animations', function (req, res) {
+	console.log(req.method);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile('./form.html', function(err,data) {
+		res.write(data);
+		res.end();
+	});
+	console.log(req.body.mode);
+	light_process.kill('SIGHUP');
+	light_process = cp.fork('child.js');
+	mode = req.body.mode;
+	light_process.send({mode:mode,
+		primary_color:primary_color,
+		secondary_color:secondary_color});
+});
+
+app.post('/colors', function (req, res) {
+	console.log(req.method);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	fs.readFile('./form.html', function(err,data) {
+		res.write(data);
+		res.end();
+	});
+	console.log(req.body.primary_color);
+	console.log(req.body.secondary_color);
+	light_process.kill("SIGHUP");
+	light_process = cp.fork('child.js');
+	light_process.send(mode)
+});
+
+
+app.listen(80, function() {
+	console.log('done');
+});
+
+light_process.on('message', function (m) {
+		console.log(m);
+});
---
+++ child.js
@@ -0,0 +1,101 @@
+var ws281x = require('rpi-ws281x-native');
+var sleep = require('sleep');
+
+var NUM_LEDS = parseInt(process.argv[2], 10) || 100,
+	 pixelData = new Uint32Array(NUM_LEDS);
+ws281x.init(NUM_LEDS);
+
+redData = new Uint32Array(NUM_LEDS);
+for(var i=0;i<NUM_LEDS;i++) {
+	redData[i] = 0xff0000;
+}
+blueData = new Uint32Array(NUM_LEDS);
+for(var i=0;i<NUM_LEDS;i++) {
+	blueData[i] = 0x00ff00;
+}
+greenData = new Uint32Array(NUM_LEDS);
+for(var i=0;i<NUM_LEDS;i++) {
+	greenData[i] = 0x0000ff;
+}
+whiteData = new Uint32Array(NUM_LEDS);
+for(var i=0;i<NUM_LEDS;i++) {
+	whiteData[i] = 0xffffff;
+}
+
+var previous = 0;
+process.on('message', function(m) {
+	console.log(m.mode);
+	console.log(m.primary_color);
+	if (m.mode == "move") {
+		runMovingStreamAnimation(m.primary_color,m.secondary_color);
+	} else if (m.mode == "red") {
+		ws281x.render(redData);
+	} else if (m.mode == "random")  {
+		runRandomPixelAnimation(m.primary_color,m.secondary_color);
+	} else if (m.mode == "off") {
+		turnOff();
+	}
+});
+
+var runMovingStreamAnimation = function (color1,color2) {
+	moveData = new Uint32Array(NUM_LEDS);
+	for(var i=0;i<NUM_LEDS;i++) {
+		moveData[i] = color2;
+	}
+	var spacing = 5;
+	var current_count = 0;
+	while (true) {
+		for (var i = 0;i<NUM_LEDS;i++) {
+			if (i % spacing == current_count) {
+				moveData[i] = color1;
+			} else {
+				moveData[i] = color2;
+			}
+		}
+		current_count += 1;
+		current_count = current_count % spacing;
+		sleep.msleep(30);
+		ws281x.render(moveData);
+		}
+	}
+var turnOff = function() {
+	pixelData = new Uint32Array(NUM_LEDS);
+	for (var i = 0; i < NUM_LEDS;i++) {
+		pixelData[i] = 0x000000;
+	}
+	ws281x.render(pixelData);
+}
+
+var runRandomPixelAnimation = function (color1,color2) {
+	var count = NUM_LEDS;
+	var index = 0;
+	pixelData = new Uint32Array(NUM_LEDS);
+	while(true) {
+		index = 0;
+		count = NUM_LEDS;
+		for (var i = 0; i < NUM_LEDS;i++) {
+			pixelData[i] = color2
+		}
+		pixelIndices = new Uint32Array(NUM_LEDS);
+
+		for (var i = 0; i < NUM_LEDS;i++) {
+			pixelIndices[i]=i;
+		}
+		while (count != 0){
+			randomIndex = Math.floor(Math.random() * count);
+			count = count - 1;
+			if (randomIndex != count) {
+				index = pixelIndices[randomIndex];
+				pixelIndices[randomIndex] = pixelIndices[count];
+				pixelData[index] = color1;
+				ws281x.render(pixelData);
+				sleep.msleep(30);
+			} else {
+				index = pixelIndices[randomIndex];
+				pixelData[index] = color1;
+				ws281x.render(pixelData);
+				sleep.msleep(30);
+			}
+		}
+	}
+}
